e1c93f95eb978fed8c96526c486ed16d2b5c8428
nilkr47 <145826752+nilkr47@users.noreply.github.com>
2025-07-14 22:06:03 +0530

    Add files via upload

---
+++ tast1.py
@@ -0,0 +1,6 @@
+a = int(input("Please Enter the first num"))
+b = int(input("Please Enter the second num"))
+print ("Addition =",a+b)
+print ("Subtraction =",a-b)
+print ("Multiplication =",a*b)
+print("Division =",a/b)
